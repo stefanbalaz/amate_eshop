b78c152c86ce83b10cf634c2279f04802c529680
stefanbalaz <stefanbalaz@hotmal.com>
2026-03-19 22:27:38 +0100

    aws-fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,14 +6,6 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
 =========================================================================*/
-// const schema = a.schema({
-//   Todo: a
-//     .model({
-//       content: a.string(),
-//     })
-//     .authorization((allow) => [allow.guest()]),
-// });
-
 const schema = a.schema({
   // ------------------------
   // PRODUCT
